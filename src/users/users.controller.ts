fad5a3b3c8f619bf26a47be3e7d9fef7773cb9c2
LaunchTrip <Fernando@Fernandos-MacBook-Pro.local>
2021-06-23 10:26:45 -0700

    started create CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,25 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body } from '@nestjs/common';
+import { Controller, Get, Post, Param } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  constructor(private usersService: UsersService) {}
   @Get()
-  getUsers(): any {
-    return [{ id: 0 }];
+  getUsers(): User[] {
+    return this.usersService.findAll();
   }
 
   @Get(':id')
-  getUsersById(@Param('id') id: string): any {
-    return id;
+  getUsersById(@Param('id') id: string): User {
+    // TODO implement auto parse
+    return this.usersService.findById(id);
+  }
+
+  @Post()
+  createUser(@Body() body: CreateUserDTO): User {
+    
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
-
+import { v4 as uuidv4 } from 'uuid';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      id: uuidv4(),
+      name: 'Fernando',
+    },
+  ];
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findById(userID: string): User {
+    return this.users.find((user) => user.id === userID);
+  }
+
+  createUser(createUserDTO: CreateUserDTO): User {
+    const newUser = {
+      id: uuidv4(),
+      ...createUserDTO,
+    };
+
+    this.users.push(newUser);
+
+    return newUser;
+  }
+}
